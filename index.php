288d5693443b6f09cde96024248d1abbc2d4d1c8
AshlavaDev <104705344+AshlavaDev@users.noreply.github.com>
2024-01-16 16:22:48 -0700

    add limits to number inputs

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!-- The index to allow for more pages if needed -->
-<!-- TODOS: Add error checking and limits and style page -->
+<!-- TODO: Add error checking and style page -->
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,16 +10,16 @@
 <body>
   <h1>FizzBuzz PHP</h1>
   <h2>A FizzBuzz program, on the web</h2>
-  <p>This program accepts 2 numbers, and returns a list of every number between them inclusive, replacing multiples of 3 with "Fizz", multiples of 5 with "Buzz", and multiples of both with "FizzBuzz".</p>
+  <p>This program accepts 2 numbers between 0 and 1000, and returns a list of every number between them inclusive, replacing multiples of 3 with "Fizz", multiples of 5 with "Buzz", and multiples of both with "FizzBuzz".</p>
   <form method="post" action="">
-    <!-- Add error checking and limits -->
+    <!-- Add error checking -->
     <div>
       <label for="start">Start:</label>
-      <input type="number" id="start" name="start">
+      <input type="number" id="start" name="start" min="0" max="1000">
     </div>
     <div>
       <label for="end">End:</label>
-      <input type="number" id="end" name="end">
+      <input type="number" id="end" name="end" min="0" max="1000">
     </div>
     <button type="submit">Submit</button>
   </form>
